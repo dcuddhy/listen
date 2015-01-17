436cf48b31417a9b20ebb495fae74403d5113033
dcuddhy <dcuddhy@gmail.com>
2015-01-16 17:27:12 -0700

    mustache is killin it with json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 $( document ).ready(function() {
 
 
+
 $('.container').on('click', '.fa-play', function() {
   $('.fa').removeClass('fa-pause').addClass('fa-play');
   $(this).removeClass('fa-play').addClass('fa-pause');
@@ -13,22 +14,15 @@ $('.container').on('click', '.fa-pause', function() {
 
 
 
+  jQuery.getJSON('data.json', function(tracks) {
 
+    var $tracksTemplate = $('#tracksTemplate').html();
 
+    var newHTML = Mustache.to_html($tracksTemplate, tracks);
+
+    $('.player').html(newHTML);
+  });
 
 
 
 });
-
-
-
-
-
-  jQuery.getJSON('data.json', function(data) {
-
-    var $tracksTemplate = $('#tracksTemplate').html();
-
-    var newHTML = Mustache.to_html($tracksTemplate, data);
-
-    $('.tracks').html(newHTML);
-  });
