bd64577a84d2e0328113435caa6740286050b1a4
dcuddhy <dcuddhy@gmail.com>
2015-01-20 11:44:54 -0700

    stop in effect.  cleaned up code. tighened up css even more

--- app.js
+++ app.js
@@ -1,72 +1,31 @@
 "use strict";
 $( document ).ready(function() {
 
-
+  // Play song! load assures start from beginning, producing
+  // stop effect.  var title sends title to header.
   $('.container').on('click', '.fa-play', function() {
-    var id = $(this).data('id')
-    document.getElementById(id).play()
+    var id = $(this).data('id');
+    document.getElementById(id).load();
+    document.getElementById(id).play();
     $('.fa').removeClass('fa-stop').addClass('fa-play');
     $(this).removeClass('fa-play').addClass('fa-stop');
-    var title = $(this).data('title')
-    $('.headZ').html("Now Playing: " + title )
+    var title = $(this).data('title');
+    $('.heading').html("Now Playing: " + title );
   });
 
+  //Stop song!  Really pause.  Stop effect implemented above.
   $('.container').on('click', '.fa-stop', function() {
-    var id = $(this).data('id')
-    document.getElementById(id).pause()
+    var id = $(this).data('id');
+    document.getElementById(id).pause();
     $(this).removeClass('fa-stop').addClass('fa-play');
-    $('.headZ').html("Select a song!")
+    $('.heading').html("Select a song!");
   });
 
+  // Directs script to player with JSON
   $.getJSON('data.json', function(tracks) {
     var $tracksTemplate = $('#tracksTemplate').html();
     var newHTML = Mustache.to_html($tracksTemplate, tracks);
     $('.player').html(newHTML);
   });
-
-  $.getJSON('data.json', function(id) {
-    var $headerTemplate = $('#headerTemplate').html();
-    var newHTML = Mustache.to_html($headerTemplate, id);
-    $('.header').html(newHTML);
-  });
-
-
-
-
-
-    // $(this).children("audio").trigger('play');
-    // $(this).children("audio").trigger('pause');
-
-
-
-
-
-
-
-
-
-
-
-
-  //
-  // document.body.onload = addElement;
-  //
-  // function addElement () {
-  //   // create a new div element
-  //   // and give it some content
-  //   var newDiv = document.createElement("div");
-  //   var newContent = document.createTextNode("Hi there and greetings!");
-  //   newDiv.appendChild(newContent); //add the text node to the newly created div.
-  //
-  //   // add the newly created element and its content into the DOM
-  //   var currentDiv = document.getElementById("header");
-  //   document.body.insertBefore(newDiv, currentDiv);
-  // }
-
-
-
-      // WILL PAUSE A JAM
-    // document.getElementById('jam').pause();
-
 
 });
